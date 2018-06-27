1e8b8d964aba8c8070f5736306d17b329229e202
Furipu <vandamme.philippe@gmail.com>
2018-06-27 22:53:13 +0200

    oefening2.3

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,7 +4,7 @@
       {{title}}
     </h1>
     <ul>
-      <li v-for(book in books)>{{book.title}}:{{book.author}}</li>
+      <li v-for="book in books">{{book.title}}:{{book.author}}</li>
     </ul>
   </div>
 </template>
